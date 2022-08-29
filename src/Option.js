11233fa36b164d9dbf7ed9d7ff198d1b8cc9c248
Lech Kulina <kulinalech@gmail.com>
2022-08-29 17:55:59 +0200

    Implement Option

---
+++ src/Option.js
@@ -0,0 +1,29 @@
+class Option {
+  constructor(name, description, type, required, autocomplete) {
+    this.name = name;
+    this.description = description;
+    this.type = type;
+    this.required = required;
+    this.autocomplete = autocomplete;
+  }
+
+  getConfig() {
+    return {
+      name: this.name,
+      description: this.description,
+      type: this.type,
+      required: this.required,
+      autocomplete: this.autocomplete,
+    };
+  }
+
+  processValue(value) {
+    return value; // some options may want to process the value in some way
+  }
+
+  async getAutoCompeteResults(interaction, dataModel) {
+    return interaction.result([]);
+  }
+};
+
+module.exports = Option;
